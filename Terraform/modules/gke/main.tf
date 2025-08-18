bd891afa542d3652736094d6d1050d5ca3af7184
Ahmed Rafat <ahmedrafaat456@example.com>
2025-08-18 15:18:45 +0300

    edit ingress nginx

--- Terraform/modules/gke/main.tf
+++ Terraform/modules/gke/main.tf
@@ -139,7 +139,7 @@ resource "google_container_node_pool" "standard_pool" {
 
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-standard-2"
     disk_size_gb = 30
     disk_type    = "pd-balanced"
 
